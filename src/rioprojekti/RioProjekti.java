890324a09218e3ae5060642040fd01a30f99b253
Joni <joni.makela@gmail.com>
2012-02-16 17:58:00 +0200

    first commit

---
+++ src/rioprojekti/RioProjekti.java
@@ -0,0 +1,13 @@
+package rioprojekti;
+
+public class RioProjekti
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+	// TODO code application logic here
+    }
+}
